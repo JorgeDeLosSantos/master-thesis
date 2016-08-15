a1fb42faabd56e08bf760ea59256677a30a32144
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2016-08-15 16:27:54 -0500

    Actualizando metodología y otros

--- toPDF.bat
+++ toPDF.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
-pdflatex -interaction=nonstopmode main.tex
-bibtex main.aux
-pdflatex -interaction=nonstopmode main.tex
-bibtex main.aux
-pdflatex -interaction=nonstopmode main.tex
-main.pdf
+pdflatex -interaction=nonstopmode tesis_mcim.tex
+bibtex tesis_mcim.aux
+pdflatex -interaction=nonstopmode tesis_mcim.tex
+bibtex tesis_mcim.aux
+pdflatex -interaction=nonstopmode tesis_mcim.tex
+tesis_mcim.pdf
 exit
