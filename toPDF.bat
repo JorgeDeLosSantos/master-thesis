41ca517f4e772316e52d469363f19227d90155d3
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2016-10-03 18:20:31 -0500

    Actualizando...

--- toPDF.bat
+++ toPDF.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 pdflatex -interaction=nonstopmode tesis_mcim.tex
-bibtex tesis_mcim.aux
-pdflatex -interaction=nonstopmode tesis_mcim.tex
-pdflatex -interaction=nonstopmode tesis_mcim.tex
+REM bibtex tesis_mcim.aux
+REM pdflatex -interaction=nonstopmode tesis_mcim.tex
+REM pdflatex -interaction=nonstopmode tesis_mcim.tex
 tesis_mcim.pdf
 exit
